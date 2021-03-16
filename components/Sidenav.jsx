a3de291c47b793560857af7c094af33a6d8e26fb
Kartik Seth <kartikseth28@gmail.com>
2021-03-16 20:00:38 +0530

    ready to deploy

--- components/Sidenav.jsx
+++ components/Sidenav.jsx
@@ -11,22 +11,22 @@ export default function SideBar() {
           <div id="icon-container" >
             <span>
                 <a href="https://www.linkedin.com/in/kartik-seth-0a8696137/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} style={{ padding: '5px', margin:'5px', color: 'var(--secondary)', fontSize: '0.5rem' }} />
+                <FontAwesomeIcon icon={faLinkedin} style={{ padding: '3px', margin:'5px', color: 'var(--secondary)', fontSize: '1.8em' }} />
                 </a>
             </span>
             <span>
                 <a href="kartikseth28@gmail.com" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faEnvelope} style={{ padding: '5px', margin:'5px',color: 'var(--secondary)', fontSize: '0.5rem' }} />
+                <FontAwesomeIcon icon={faEnvelope} style={{ padding: '3px', margin:'5px',color: 'var(--secondary)', fontSize: '1.8em' }} />
                 </a>
             </span>
             <span>
                 <a href="https://github.com/Kartik-ksks" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} style={{ padding: '5px',margin:'5px', color: 'var(--secondary)', fontSize: '0.5rem' }} />
+                <FontAwesomeIcon icon={faGithub} style={{ padding: '3px',margin:'5px', color: 'var(--secondary)', fontSize: '1.8em' }} />
                 </a>
             </span>
             <span>
                 <a href="https://www.facebook.com/kartik.seth.54/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faFacebook} style={{ padding: '5px',margin:'5px', color: 'var(--secondary)', fontSize: '0.5rem' }} />
+                <FontAwesomeIcon icon={faFacebook} style={{ padding: '3px',margin:'5px', color: 'var(--secondary)', fontSize: '1.8em' }} />
                 </a>
             </span>
           </div>
